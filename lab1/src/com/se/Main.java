aba8f1da20a914548147c8f45c0135c595dec580
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-02-24 17:47:46 +0200

    updates

--- lab1/src/com/se/Main.java
+++ lab1/src/com/se/Main.java
@@ -3,6 +3,16 @@ package com.se;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world!");
+        String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
+        int n = (int) (Math.random() * 1_000_000);
+        n*=3;
+        int i = (byte) 10101;
+        n+=i;
+        String nrhex = "FF";
+        int dec = Integer.parseInt(nrhex, 16);
+        n+=dec;
+        n*=6;
+        System.out.println(n);
     }
 }

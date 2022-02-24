838c9496557482ee268aa3a2004857001669b8a5
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-02-24 18:57:53 +0200

    final fix

--- lab1-comp/src/com/se/Main.java
+++ lab1-comp/src/com/se/Main.java
@@ -2,31 +2,34 @@ package com.se;
 
 public class Main {
 
+    public static int sumDig(int n) {
+        int rezult = 0;
+        while (n > 0) {
+            rezult += n % 10;
+            n /= 10;
+        }
+        return rezult;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String[] languages = {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"};
         int n = (int) (Math.random() * 1_000_000);
-        n*=3;
+
         String input0 = "10101";
         int i = Integer.parseInt(input0 , 2);
-        n+=i;
+
         String nrhex = "FF";
         int dec = Integer.parseInt(nrhex, 16);
-        n+=dec;
-        n*=6;
-        int finrez = 0;
-        while(n>0)
-        {
-            int decc = n%10;
-            finrez+=decc;
-            n/=10;
+        int finalRez = 0;
+        int rezz = (n*3 + i + dec)*6;
+        if(sumDig(rezz)>9){
+            int auxVal = sumDig(rezz);
+            finalRez = sumDig(auxVal);
         }
-        int rez = 0;
-        while (finrez>9){
-            int decc = finrez%10;
-            rez+=decc;
-            finrez/=10;
-        }
-        System.out.println("Willy-nilly, this semester I will learn " + languages[rez]);
+        System.out.println("Willy-nilly, this semester I will learn " + languages[finalRez]);
+        //System.out.println("Willy-nilly, this semester I will learn " + languages[rez]);
     }
 }

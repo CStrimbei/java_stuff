3e5825196545a31b23901c31207896aef333cac3
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-02-28 18:12:25 +0200

    Homework p2

--- lab1-comp/src/com/se/Homework.java
+++ lab1-comp/src/com/se/Homework.java
@@ -5,16 +5,22 @@ import java.util.Scanner;
 public class Homework {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
-        if(args.length>=3){
-            int n = Integer.parseInt(args[0]);
-            int p = Integer.parseInt(args[1]);
+        int n = 0;
+        int p = 0;
+        if (args.length >= 3) {
+            n = Integer.parseInt(args[0]);
+            p = Integer.parseInt(args[1]);
             System.out.println("Numarul de argumente este valid!");
-        }
-        else if(args.length == 2){
+        } else if (args.length == 2) {
             System.out.println("Ai introdus doar primele 2 argumente!");
-        } else if(args.length <2 ){
+        } else if (args.length < 2) {
             System.out.println("Ai introdus prea putine argumente!");
         }
-
+        char[] words = new char[args.length-2];
+        for (int i = 2; i < args.length; i++){
+            words[i-2]=args[i].charAt(0);
+        }
+        System.out.println(words);
     }
+
 }

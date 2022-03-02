e7f013851e511a44dfdc8e670179dead51b5aade
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-03-02 23:41:51 +0200

    Compulsory activity

---
+++ lab2 - comp/src/com/se/Main.java
@@ -0,0 +1,35 @@
+package com.se;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Events> evnt = new ArrayList<>();
+        List<Rooms> room = new ArrayList<>();
+
+        evnt.add(new Events(100, 8, 10, "C1"));
+        evnt.add(new Events(100, 10, 12, "C2"));
+        evnt.add(new Events(30, 8, 10, "L1"));
+        evnt.add(new Events(30, 8, 10, "L2"));
+        evnt.add(new Events(30, 10, 12, "L3"));
+
+        room.add(new Rooms(30, Types.lab, "401"));
+        room.add(new Rooms(30, Types.lab, "403"));
+        room.add(new Rooms(30, Types.lab, "405"));
+        room.add(new Rooms(30, Types.lecture_hall, "401"));
+
+        System.out.print("Events: ");
+
+        for(Events e : evnt)
+            System.out.print(e + " ");
+
+        System.out.println();
+
+        System.out.print("Rooms: ");
+        for(Rooms r : room)
+            System.out.print(r + " ");
+    }
+}

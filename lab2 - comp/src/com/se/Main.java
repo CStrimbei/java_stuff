10415e16e692237ac7d1a99e9590d3a54713f0fd
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-03-10 11:09:45 +0200

    Inst

--- lab2 - comp/src/com/se/Main.java
+++ lab2 - comp/src/com/se/Main.java
@@ -7,29 +7,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Events> event = new ArrayList<>();
-        List<Rooms> room = new ArrayList<>();
-
-        event.add(new Events(100, 8, 10, "C1"));
-        event.add(new Events(100, 10, 12, "C2"));
-        event.add(new Events(30, 8, 10, "L1"));
-        event.add(new Events(30, 8, 10, "L2"));
-        event.add(new Events(30, 10, 12, "L3"));
-
-        room.add(new Rooms(30, Types.lab, "401"));
-        room.add(new Rooms(30, Types.lab, "403"));
-        room.add(new Rooms(30, Types.lab, "405"));
-        room.add(new Rooms(30, Types.lecture_hall, "401"));
-
-        System.out.print("Events: ");
-
-        for(Events e : event)
-            System.out.print(e + " ");
-
-        System.out.println();
-
-        System.out.print("Rooms: ");
-        for(Rooms r : room)
-            System.out.print(r + " ");
     }
 }

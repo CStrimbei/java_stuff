106e93b23f34ba604ef86f0c8107923a8c5f846f
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-03-16 20:18:43 +0200

    Better adding of the edges, making the node list without dupes

--- lab3 - comp/src/com/se/Main.java
+++ lab3 - comp/src/com/se/Main.java
@@ -4,18 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 	Network n = new Network();
-    n.addNodes(new Computer("Computer 1", "192.168.1.1", 1000), new Router("Router 1", "192.168.0.1", "00:00:5e:00:53:af"), 10);
-    /*n.addNode(new Computer("Computer 2", "192.168.2.1";
 
-    n.addNode());
-    n.addNode(new Router("Router 2", "192.168.6.2", "10:03:5e:00:53:af"));
-    n.addNode(new Switch("Switch 1", "Iasi"));
-    n.addNode(new Switch("Switch 2", "Pascani"));*/
+    Node v1 = new Computer("Computer A", "192.168.1.1", 1000);
+    Node v2 = new Router("Router A", "192.168.0.1", "00:00:5e:00:53:af");
+    Node v3 = new Switch("Switch A", "Iasi");
+    Node v4 = new Switch("Switch B", "Pascani");
+    Node v5 = new Router("Router B", "192.168.6.2", "10:03:5e:00:53:af");
+    Node v6 = new Computer("Computer B", "192.168.2.1", 512);
 
+   /* n.addNode(v1);
+    n.addNode(v2);
+    n.addNode(v3);
+    n.addNode(v4);
+    n.addNode(v5);
+    n.addNode(v6);*/
+
+    n.addEdge(v1, v2, 10);
+    n.addEdge(v1, v3, 50);
+    n.addEdge(v2, v3, 20);
+    n.addEdge(v2, v4, 20);
+    n.addEdge(v2, v5, 20);
+    n.addEdge(v3, v4, 10);
+    n.addEdge(v4, v5, 30);
+    n.addEdge(v4, v6, 10);
+    n.addEdge(v5, v6, 20);
+        System.out.println("The following nodes are in the system: ");
 
         for (Node node:n.nodeList) {
-            System.out.println("I have found the node with the name: "+node);
+            System.out.println(node);
         }
+
+        System.out.println("And these are the costs: ");
+
         System.out.println(n.costs);
     }
 }
--- lab3 - comp/src/com/se/Network.java
+++ lab3 - comp/src/com/se/Network.java
@@ -8,11 +8,24 @@ import java.util.Map;
 public class Network {
     List<Node> nodeList= new ArrayList<>();
     Map<String, Integer> costs = new HashMap<String, Integer>();
-    public void addNodes(Node node1, Node node2, int COST){
-        nodeList.add(node1);
-        nodeList.add(node2);
+
+    public void addNode(Node node){
+        nodeList.add(node);
+    }
+
+    public void addEdge(Node node1, Node node2, int COST){
         String Edge = node1 + " -> " + node2;
         costs.put(Edge, COST);
+
+        if(nodeList.contains(node1))
+                if(nodeList.contains(node2))
+                    return;
+                else {
+                    nodeList.add(node2);
+                }
+        else {
+            nodeList.add(node1);
+        }
     }
 
     @Override

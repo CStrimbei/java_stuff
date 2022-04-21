cdd4ecfd58fe591152baed0cfb4d1b5fb2512051
Strimbei Claudiu-Catalin <75887924+CStrimbei@users.noreply.github.com>
2022-04-21 18:04:33 +0300

    leftover stuff

--- lab6-comp/src/main/java/com/lab6/HelloController.java
+++ lab6-comp/src/main/java/com/lab6/HelloController.java
@@ -4,10 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    protected void onHelloButtonClick() {
-        System.out.println("Welcome to JavaFX Application!");
-    }
 
     @FXML
     protected void onExitButtonClick(){
